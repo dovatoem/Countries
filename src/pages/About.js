68b53f0819fff90eaf99cc7b2103c9e503f4e702
Dobrinka <dmengova@gmail.com>
2023-07-06 22:24:35 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, nesciunt accusantium. Aliquid doloremque libero fuga similique assumenda facilis saepe aut? At iste eligendi accusantium consectetur nisi illum minus reprehenderit vitae harum aperiam vero nobis quaerat non nam in quibusdam qui, omnis suscipit voluptatem odio deserunt eius! Corrupti expedita vitae tenetur quia asperiores nemo ipsam tempore temporibus consequuntur. Aut cum sint tenetur nulla dolorum obcaecati officiis dicta. Eius, sed maiores. Eligendi nulla laborum illum et nobis ex, optio eius, eum voluptatibus repellat, enim at! Officiis amet, incidunt consectetur commodi cupiditate, odio ut ea veritatis nam sunt fuga et recusandae facilis similique!</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatum voluptates laboriosam exercitationem dignissimos rem! Maiores ex facilis non mollitia debitis, sed voluptatum natus dolor accusamus eum optio nemo laborum repellendus tempore impedit aperiam suscipit ipsam totam. Blanditiis doloribus quibusdam saepe perspiciatis vero quo nobis mollitia, delectus suscipit enim iste iusto hic earum, dolore quisquam aperiam molestiae reiciendis totam obcaecati consectetur commodi nostrum ea! Assumenda nihil eius omnis saepe. Ex enim, non porro esse aspernatur nesciunt perferendis deleniti recusandae impedit culpa, ratione dicta voluptates, temporibus reiciendis architecto inventore. Ducimus ratione eaque obcaecati molestias porro quaerat voluptate reiciendis. Possimus facere atque minus vitae rerum, nisi fugit ut, dolorem veritatis laudantium non necessitatibus. Animi magni accusantium vitae illum incidunt necessitatibus odio distinctio, unde vero beatae quo iste repellendus voluptate minima culpa debitis nulla nisi enim doloremque deleniti ullam, eligendi hic! Quidem dolor et vero fuga in, voluptate veniam vitae quae, nulla perspiciatis nisi. Deserunt, dicta vel rerum similique, commodi omnis molestias consequuntur sed corrupti, quas unde fuga accusantium consectetur officiis quidem ad excepturi nam repudiandae tempore? Reprehenderit explicabo asperiores numquam nobis. Alias facilis consectetur esse ab pariatur, suscipit omnis, deserunt magni sapiente, odit reiciendis qui autem necessitatibus sequi id earum repudiandae commodi?</p>
+        </div>
+    );
+};
+
+export default About;
